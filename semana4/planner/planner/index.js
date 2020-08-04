d50c4b2c7daaf25948038148196b7300c7ba52f3
Luccas Barros <ldbcandido@gmail.com>
2020-08-04 17:00:36 -0300

    Exercícios com Desafio. Faltando o 4 e o 5

--- semana4/planner/planner/index.js
+++ semana4/planner/planner/index.js
@@ -1,52 +1,69 @@
 function createTask(){
     const taskInput = document.getElementById("tarefa")
     let inputValue = taskInput.value
-    const diasDaSemana = document.getElementById("dias-semana").value
-    // const mySelect = document.getElementsByTagName("option")[diasDaSemana].value
+    let diasDaSemana = document.getElementById("dias-semana").value
 
+    if(inputValue) {
 
-    switch(diasDaSemana) {
-        case "domingo":
-            const domingo = document.getElementById("domingo")
-            domingo.innerHTML += `<li>${inputValue}</li>`
-            taskInput.value = ""
-            break
-        
-        case "segunda":
-            const segunda = document.getElementById("segunda")
-            segunda.innerHTML += `<li>${inputValue}</li>`
-            taskInput.value = ""
-            break
-        
-        case "terca":
-            const terca = document.getElementById("terca")
-            terca.innerHTML += `<li>${inputValue}</li>`
-            taskInput.value = ""
-            break
-        
-        case "quarta":
-            const quarta = document.getElementById("quarta")
-            quarta.innerHTML += `<li>${inputValue}</li>`
-            taskInput.value = ""
-            break
-        
-        case "quinta":
-            const quinta = document.getElementById("quinta")
-            quinta.innerHTML += `<li>${inputValue}</li>`
-            taskInput.value = ""
-            break
-
-        case "sexta":
-            const sexta = document.getElementById("sexta")
-            sexta.innerHTML += `<li>${inputValue}</li>`
-            taskInput.value = ""
-            break
-        
-        case "sabado":
-            const sabado = document.getElementById("sabado")
-            sabado.innerHTML += `<li>${inputValue}</li>`
-            taskInput.value = ""
-            break
-        
+        switch(diasDaSemana) {
+            case "domingo":
+                const domingo = document.getElementById("domingo")
+                domingo.innerHTML += `<li class="tarefa-adicionada" onclick="risca(event)">${inputValue}</li>`
+                taskInput.value = ""
+                break
+            
+            case "segunda":
+                const segunda = document.getElementById("segunda")
+                segunda.innerHTML += `<li class="tarefa-adicionada" onclick="risca(event)">${inputValue}</li>`
+                taskInput.value = ""
+                break
+            
+            case "terca":
+                const terca = document.getElementById("terca")
+                terca.innerHTML += `<li class="tarefa-adicionada" onclick="risca(event)">${inputValue}</li>`
+                taskInput.value = ""
+                break
+            
+            case "quarta":
+                const quarta = document.getElementById("quarta")
+                quarta.innerHTML += `<li class="tarefa-adicionada" onclick="risca(event)">${inputValue}</li>`
+                taskInput.value = ""
+                break
+            
+            case "quinta":
+                const quinta = document.getElementById("quinta")
+                quinta.innerHTML += `<li class="tarefa-adicionada" onclick="risca(event)">${inputValue}</li>`
+                taskInput.value = ""
+                break
+    
+            case "sexta":
+                const sexta = document.getElementById("sexta")
+                sexta.innerHTML += `<li class="tarefa-adicionada" onclick="risca(event)">${inputValue}</li>`
+                taskInput.value = ""
+                break
+            
+            case "sabado":
+                const sabado = document.getElementById("sabado")
+                sabado.innerHTML += `<li class="tarefa-adicionada" onclick="risca(event)">${inputValue}</li>`
+                taskInput.value = ""
+                break
+            
+        }
+    
+    } else {
+        alert('Insira um valor válido.')
     }
+
 }   
+
+function risca(event) {
+    const tarefaClicada = event.target
+    tarefaClicada.classList.toggle("tarefa-riscada")
+}
+
+function taskRemover() {
+    const tasks = document.getElementsByTagName("ul")
+    for(let i = tasks.length -1; i >= 0; i-- ) {
+        tasks[i].remove()
+    }
+}
