7a04d2f42b0d44bcab2748efe3a7fb5e8c10bdba
Luccas Barros <ldbcandido@gmail.com>
2020-08-07 12:43:02 -0300

    Exercícios de interpretação

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,11 @@
+// ---------- INTERPRETAÇÃO DE CÓDIGO -----------
+
+// 1.
+
+/* Foi criado uma função que converte dolar em real de acordo com a entrada do usuário da cotação do dolar atual.
+No console é impresso o valor de 100 * a cotação que o usuário inserir.
+*/
+
+// 2.
+
+/*  */
