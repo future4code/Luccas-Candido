36f3590b663e412bd02529f44abdb9f73d31bdc7
Luccas Barros <ldbcandido@gmail.com>
2020-08-13 16:10:26 -0300

    Estilos Adicionados

--- semana5/instaReact/src/App.js
+++ semana5/instaReact/src/App.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
+import styled from 'styled-components'
 import logo from './'
+
+const InputTeste = styled.input`
+    background-color: #F1F1F3;
+    -webkit-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+    -moz-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+    box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+    border: none;
+    width: 300px;
+    height: 35px;
+    margin-top: 30px;
+    outline: none;`
+  
+
+  const ButtonTeste = styled.button`
+    background-color: #964F8D;
+    margin: 20px 0;
+    text-decoration: none;
+    border: none;
+    border-radius: 10px;
+    padding: 10px 30px;
+    color: white;
+    font-size: 12px;
+    cursor: pointer;
+    text-transform: uppercase;
+    letter-spacing: 0.1em;
+    font-weight: bold;
+    -webkit-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+    -moz-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+    box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);`
 
 class App extends React.Component {
 
@@ -26,6 +56,7 @@ class App extends React.Component {
     valorInputUserPhoto: "",
     valorInputPostPhoto: ""
   }
+  
 
   onChangeInputUser = (event) => {
     this.setState({valorInputUser: event.target.value})
@@ -38,6 +69,7 @@ class App extends React.Component {
   onChangeInputPostPhoto = (event) => {
     this.setState({valorInputPostPhoto: event.target.value})
   }
+  
 
 
   adicionaPost = () => {
@@ -61,6 +93,7 @@ class App extends React.Component {
   }
 
 
+
   render() {
 
     const listaPost = this.state.posts.map((post) => {
@@ -80,22 +113,22 @@ class App extends React.Component {
     })
     return (
       <div className={'app-container'}>
-        <input
+        <InputTeste
         value={this.state.valorInputUser}
         onChange={this.onChangeInputUser}
         placeholder={"Insira o nome de usuário"}
         />
-        <input
+        <InputTeste
         value={this.state.valorInputUserPhoto}
         onChange={this.onChangeInputUserPhoto}
         placeholder={"Insira o link da sua foto de usuário"}
         />
-        <input
+        <InputTeste
         value={this.state.valorInputPostPhoto}
         onChange={this.onChangeInputPostPhoto}
         placeholder={"Insira o link da foto do seu post"}
         />
-        <button onClick={this.adicionaPost}>Adicionar Post</button>
+        <ButtonTeste onClick={this.adicionaPost}>Adicionar Post</ButtonTeste>
         <div>{listaPost}</div>
 
       </div>
