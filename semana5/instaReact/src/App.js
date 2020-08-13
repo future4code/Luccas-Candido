265468c5bc376b40848caa92503a7848ac9b5ee7
Luccas Barros <ldbcandido@gmail.com>
2020-08-13 19:39:42 -0300

    Estilizando Header e Footer

--- semana5/instaReact/src/App.js
+++ semana5/instaReact/src/App.js
@@ -4,19 +4,50 @@ import Post from './components/Post/Post';
 import styled from 'styled-components'
 import logo from './'
 
-const InputTeste = styled.input`
-    background-color: #F1F1F3;
-    -webkit-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
-    -moz-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
-    box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
-    border: none;
-    width: 300px;
-    height: 35px;
-    margin-top: 30px;
-    outline: none;`
+const HeaderStyle = styled.header`
+background: #964F8D;
+width: 100%;
+height: 8vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const HeaderTitle = styled.p`
+color: white;
+font-size: 12px;
+`
+
+const FooterStyle = styled.footer`
+background: #784071;
+width: 100%;
+height: 5vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const InputStyle = styled.input`
+  background-color: #F1F1F3;
+  -webkit-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+  -moz-box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+  box-shadow: 0px 5px 5px -2px rgba(0,0,0,0.25);
+  border: none;
+  width: 300px;
+  height: 35px;
+  margin-top: 30px;
+  outline: none;`
+
+const TitleHeader = styled.h1`
+font-size: 2rem;
+color: white;
+font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+text-align: center;
+`
+
   
 
-  const ButtonTeste = styled.button`
+const ButtonStyle = styled.button`
     background-color: #964F8D;
     margin: 20px 0;
     text-decoration: none;
@@ -116,24 +147,25 @@ class App extends React.Component {
 
     return (
       <div className={'app-container'}>
-        <InputTeste
+        <HeaderStyle><TitleHeader>InstaReact</TitleHeader></HeaderStyle>
+        <InputStyle
         value={this.state.valorInputUser}
         onChange={this.onChangeInputUser}
         placeholder={"Insira o nome de usuário"}
         />
-        <InputTeste
+        <InputStyle
         value={this.state.valorInputUserPhoto}
         onChange={this.onChangeInputUserPhoto}
         placeholder={"Insira o link da sua foto de usuário"}
         />
-        <InputTeste
+        <InputStyle
         value={this.state.valorInputPostPhoto}
         onChange={this.onChangeInputPostPhoto}
         placeholder={"Insira o link da foto do seu post"}
         />
-        <ButtonTeste onClick={this.adicionaPost}>Adicionar Post</ButtonTeste>
+        <ButtonStyle onClick={this.adicionaPost}>Adicionar Post</ButtonStyle>
         <div>{listaPost}</div>
-
+        <FooterStyle><HeaderTitle>Esse projeto foi criado com React utilizando Styled Components, no bootcamp da Labenu!</HeaderTitle></FooterStyle>
       </div>
     );
   }
