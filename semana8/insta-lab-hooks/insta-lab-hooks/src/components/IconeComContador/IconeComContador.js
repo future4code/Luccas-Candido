a3e54e8a0dc26a87be70246be0b9b2eef098316d
luccasbarros <ldbcandido@gmail.com>
2020-09-08 13:01:33 -0300

    exercícios realizados

--- semana8/insta-lab-hooks/insta-lab-hooks/src/components/IconeComContador/IconeComContador.js
+++ semana8/insta-lab-hooks/insta-lab-hooks/src/components/IconeComContador/IconeComContador.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const IconeContainer = styled.div`
-	display: flex;
+  display: flex;
 
-	img {
-		margin-right: 5px;
-	}
-`
+  img {
+    margin-right: 5px;
+  }
+`;
 
 function IconeComContador(props) {
-	return <IconeContainer>
-		<img alt={'Icone'} src={props.icone} onClick={props.onClickIcone}/>
-		<p>{props.valorContador}</p>
-	</IconeContainer>
+  return (
+    <IconeContainer>
+      <img alt={"Icone"} src={props.icone} onClick={props.onClickIcone} />
+      <p>{props.valorContador}</p>
+    </IconeContainer>
+  );
 }
 
-export default IconeComContador
+export default IconeComContador;
