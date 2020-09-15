ce300ec567651254b4d9f8dfa46be1d7da81eaa4
luccasbarros <ldbcandido@gmail.com>
2020-09-15 14:27:31 -0300

    styled components homeScreen

--- semana10/lubax/src/App.js
+++ semana10/lubax/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Router from "./router/Router";
 
 function App() {
   return (
     <div className="App">
-      oi
-          </div>
+      <Router />
+    </div>
   );
 }
 
