82cb6e2225f78bd553d51395cc442abe538946b3
luccasbarros <ldbcandido@gmail.com>
2020-09-28 16:34:51 -0300

    estrutura iniciada

--- semana11/exercicio-tarde-testes-react/src/App.test.js
+++ semana11/exercicio-tarde-testes-react/src/App.test.js
@@ -5,24 +5,32 @@ import App from "./App";
 
 // 1.
 test("Confirmar posts sendo inseridos", () => {
-  const { getByText, getByPlaceholderText, queryAllByTestId } = render(<App />);
+  const { getByText, getByPlaceholderText, getAllByText } = render(<App />);
 
   const text = getByPlaceholderText(/Novo Post/i);
   const button = getByText(/Adicionar/i);
 
   fireEvent.change(text, { target: { value: "teste" } });
 
-  const post = queryAllByTestId("teste");
-
   fireEvent.click(button);
 
+  // Quantidade de posts
   let contador = getByText(/Quantidade/i);
 
   expect(contador).toHaveTextContent("Quantidade de Post: 1");
-  expect(post).not.toBeNull();
 
   fireEvent.change(text, { target: { value: "teste2" } });
   fireEvent.click(button);
 
+  const post = getByText("teste2");
+
+  expect(post).toBeInTheDocument();
   expect(contador).toHaveTextContent("Quantidade de Post: 2");
+
+  // Botão Curtir
+  const likeButton = getAllByText("Curtir");
+
+  fireEvent.click(likeButton[1]);
+
+  expect(likeButton[1]).toHaveTextContent("Descurtir");
 });
