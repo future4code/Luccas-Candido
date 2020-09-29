344ffe31c5f347ab2778ef248c7dfcbc98c94ae7
luccasbarros <ldbcandido@gmail.com>
2020-09-29 15:58:37 -0300

    estrutura post ok

--- semana12/leddit/src/components/Feed/CardPost.js
+++ semana12/leddit/src/components/Feed/CardPost.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-import {} from "./styles";
+import { goToSignUpPage } from "../../router/goToPages";
+
+import {
+  PostContainer,
+  DivUpDown,
+  DetailContainer,
+  CommentsContainer,
+  Icon,
+  ArrowUp,
+  ArrowDown,
+} from "./styles";
 
 function CardPost() {
-  return <div>oi</div>;
+  const history = useHistory();
+
+  return (
+    <PostContainer>
+      <DivUpDown>
+        <ArrowUp />
+        <p>123k</p>
+        <ArrowDown />
+      </DivUpDown>
+
+      <DetailContainer>
+        <h1>Post exemplo xD</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          efficitur pretium nulla ac elementum. Duis fermentum non ipsum ut
+          suscipit. Sed at ligula eu arcu mollis convallis sit amet a orci. Nam
+          dapibus dolor et est fringilla, at maximus dolor dictum. Praesent
+          fermentum ligula nec est vestibulum, vel pharetra nulla scelerisque.
+          Donec tincidunt quis augue ac viverra. Cras congue bibendum nibh, a
+          aliquam sem molestie in.
+        </p>
+        <CommentsContainer>
+          <p>
+            <Icon onClick={() => goToSignUpPage(history)} /> 0 comentários
+          </p>
+        </CommentsContainer>
+      </DetailContainer>
+    </PostContainer>
+  );
 }
 
 export default CardPost;
--- semana12/leddit/src/components/Feed/styles.js
+++ semana12/leddit/src/components/Feed/styles.js
@@ -1,5 +1,32 @@
 import styled from "styled-components";
 
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+
+export const Icon = styled(ChatBubbleIcon)`
+  cursor: pointer;
+  color: #878a8c;
+  margin-right: 1rem;
+  &:hover {
+    background-color: #efefef;
+  }
+`;
+
+export const ArrowUp = styled(ArrowUpwardIcon)`
+  cursor: pointer;
+  &:hover {
+    background-color: #efefef;
+  }
+`;
+
+export const ArrowDown = styled(ArrowDownwardIcon)`
+  cursor: pointer;
+  &:hover {
+    background-color: #efefef;
+  }
+`;
+
 export const ContainerAll = styled.div`
   display: flex;
   justify-content: center;
@@ -11,4 +38,34 @@ export const ContainerPost = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+`;
+
+export const PostContainer = styled.div`
+  display: flex;
+  height: auto;
+  width: 50%;
+  border: 1px solid black;
+`;
+
+export const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: inherit;
+`;
+
+export const CommentsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  margin-left: 2rem;
+  align-items: center;
+`;
+
+export const DivUpDown = styled.div`
+  width: 15vw;
+  height: auto;
+  background-color: #f8f9fa;
+  flex-direction: column;
+  align-items: center;
+  display: flex;
 `;
