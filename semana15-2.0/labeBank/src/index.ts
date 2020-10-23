3dfcb2f7ef9127364cb9e95f1b3889e92a95063e
luccasbarros <ldbcandido@gmail.com>
2020-10-23 13:30:29 -0300

    inserção de saldo no usuário

--- semana15-2.0/labeBank/src/index.ts
+++ semana15-2.0/labeBank/src/index.ts
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+enum BALANCE {
+  BALANCE = 0
+}
 
 type trasactions = {
   value: number,
@@ -17,19 +20,19 @@ type trasactions = {
 
 type user = {
   id: number,
+  cpf: number,
   name: string,
-  age: number
+  age: number,
+  balance: BALANCE.BALANCE,
+  account: accountType
 }
 
-type conta = {
+type accountType = {
   extract: trasactions[]
 }
 
-const users:user[] = [] 
+const users:user[] = []
 
-const contas:conta[] = []
-
-const deal = []
 
 
 
@@ -45,36 +48,95 @@ app.get("/users", (req:Request, res:Response) => {
 
 })
 
+// Usuário 
+app.get("/users/:cpf", (req:Request, res:Response) => {
+
+  try {
+
+    const userByCpf = users.filter((u) => {
+      return u.cpf === Number(req.params.cpf)
+    })
+
+    const balanceUser = userByCpf.map((user) => {
+      return user.balance
+    })
+
+    if(userByCpf.length === 0) {
+      res.status(404).send({message: "User not found!"})
+    } else {
+      res.status(200).send(balanceUser)
+    }
+
+
+  } catch(error) {
+    res.status(400).send({message:"Error inserting data!"})
+  }
+
+})
 
 // Criar usuário!
 app.post("/users", (req:Request, res:Response) => {
 
   try {
 
-    const {id, name, age} = req.body
+    const {id,cpf, name, age} = req.body
 
     const user:user = {
       id: id,
+      cpf: cpf,
       name: name,
-      age: age
+      age: age,
+      balance: BALANCE.BALANCE,
+      account: {
+        extract: []
+      }
     }
 
     const userExist = users.find((u) => {
-      return u.id === user.id
+      return u.id === user.id || u.cpf === user.cpf
     })
 
     if(userExist) {
       res.status(400).send({message: "User already exist"})
-    } else if(user.age > 18) {
+    } 
+    
+      else if(user.age > 18) {
       users.push(user)
       res.status(200).send({message: "User created sucesfully"})
-    } else {
+    } 
+
+      else {
       res.status(400).send({message: "User under age!"})
     }
 
 
   } catch(error) {
     res.status(400).send({message: "Erro Inserting Data"})
+  }
+
+})
+
+// Adicionar saldo ao usuário
+app.put("/users", (req: Request, res:Response) => {
+
+  try {
+
+    const {name, cpf, value} = req.body
+
+    const userCpf = users.findIndex((u) => {
+      return u.cpf === cpf && u.name === name
+    })
+
+    if(userCpf === -1) {
+      throw new Error()
+    }
+
+    users[userCpf].balance += value
+
+    res.status(200).send({message: `Added ${value} to user balance!`})
+
+  } catch(error) {
+    res.status(404).send({message: "Error inserting data!"})
   }
 
 })
