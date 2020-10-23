3366ea1614150e4adeea34ee257dee3cbb2188dd
luccasbarros <ldbcandido@gmail.com>
2020-10-23 12:17:40 -0300

    validação idade no create user

--- semana15-2.0/labeBank/src/index.ts
+++ semana15-2.0/labeBank/src/index.ts
@@ -8,6 +8,77 @@ app.use(express.json());
 app.use(cors());
 
 
+type trasactions = {
+  value: number,
+  date: Date,
+  description: string
+}
+
+
+type user = {
+  id: number,
+  name: string,
+  age: number
+}
+
+type conta = {
+  extract: trasactions[]
+}
+
+const users:user[] = [] 
+
+const contas:conta[] = []
+
+const deal = []
+
+
+
+// Pegar usuário
+app.get("/users", (req:Request, res:Response) => {
+
+  try {
+    res.status(200).send(users)
+
+  } catch(error) {
+    res.status(400).send({message: "Erro Inserting Data"})
+  }
+
+})
+
+
+// Criar usuário!
+app.post("/users", (req:Request, res:Response) => {
+
+  try {
+
+    const {id, name, age} = req.body
+
+    const user:user = {
+      id: id,
+      name: name,
+      age: age
+    }
+
+    const userExist = users.find((u) => {
+      return u.id === user.id
+    })
+
+    if(userExist) {
+      res.status(400).send({message: "User already exist"})
+    } else if(user.age > 18) {
+      users.push(user)
+      res.status(200).send({message: "User created sucesfully"})
+    } else {
+      res.status(400).send({message: "User under age!"})
+    }
+
+
+  } catch(error) {
+    res.status(400).send({message: "Erro Inserting Data"})
+  }
+
+})
+
 
 
 // Rodar servidor
